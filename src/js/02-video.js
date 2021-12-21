8f7ec74435f34a56fd6e848610d86f49813eec9e
Oleksandr Vaskevych <oleksandrvaskevich@gmail.com>
2021-12-21 13:16:30 +0200

    files

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(function(event) {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(event.seconds));
+},1000));
+
+const response = localStorage.getItem(LOCALSTORAGE_KEY);
+const timeStart = JSON.parse(response);
+
+player.setCurrentTime(timeStart).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,57 @@
+import throttle from 'lodash.throttle';
+
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+
+}
+
+const LOCALSTORAGE_KEY_FORM = "feedback-form-state";
+
+
+
+
+refs.form.addEventListener('input', throttle(onFormInput,500));
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+
+const formData = {};
+
+function onFormInput(e) {
+    formData[e.target.name] = e.target.value;
+
+    localStorage.setItem(LOCALSTORAGE_KEY_FORM, JSON.stringify(formData));
+    
+}
+
+function onFormSubmit (e) {
+    e.preventDefault();
+    localStorage.getItem(LOCALSTORAGE_KEY_FORM);
+    e.target.reset();
+    localStorage.removeItem(LOCALSTORAGE_KEY_FORM);
+    console.log(formData);
+}
+  
+
+document.addEventListener("DOMContentLoaded", function() { 
+    document.querySelectorAll('textarea, input').forEach(function(e) {
+
+        // если данные значения уже записаны в sessionStorage, то вставляем их в поля формы
+        // путём этого мы как раз берём данные из памяти браузера, если страница была случайно перезагружена
+
+        if(e.value === '') e.value = localStorage.getItem(e.name, e.value);
+
+        // на событие ввода данных (включая вставку с помощью мыши) вешаем обработчик
+        
+        e.addEventListener('input', function() {
+            
+            // и записываем в sessionStorage данные, в качестве имени используя атрибут name поля элемента ввода
+            
+            localStorage.setItem(e.name, e.value);
+        });
+        
+    })
+    
+}); 
 
