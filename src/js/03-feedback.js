69e9aa36f338123cdc023f729761f21882ace6dc
Oleksandr Vaskevych <oleksandrvaskevich@gmail.com>
2021-12-28 16:16:02 +0200

    full version

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,52 +1,32 @@
 import throttle from 'lodash.throttle';
 
-
-const refs = {
-    form: document.querySelector('.feedback-form'),
-
-}
+const form = document.querySelector('.feedback-form');
 
 const LOCALSTORAGE_KEY_FORM = "feedback-form-state";
+const getItems = localStorage.getItem(LOCALSTORAGE_KEY_FORM);
+const data = JSON.parse(getItems); 
+let formData = data || {};
+
+form.addEventListener('input', throttle(onFormInput,500));
+form.addEventListener('submit', onFormSubmit);
 
 
-
-refs.form.addEventListener('input', throttle(onFormInput,500));
-
-refs.form.addEventListener('submit', onFormSubmit);
-
-
-const formData = {
-};
-
-loadPage();
+if (data && data.email) {
+    form.email.value = formData.email;
+}
+if (data && data.message) {
+    form.message.value = formData.message;
+}
 
 function onFormInput(e) {
     formData[e.target.name] = e.target.value;
-
     localStorage.setItem(LOCALSTORAGE_KEY_FORM, JSON.stringify(formData));
-    
 }
-
-
-
- 
-function loadPage() {
-    const getItems = localStorage.getItem(LOCALSTORAGE_KEY_FORM);
-    const data = JSON.parse(getItems); 
-        if (data) {
-            formData.email = data.email;
-            formData.message = data.message;
-            refs.form.email.value = formData.email;
-            refs.form.message.value = formData.message;
-        }
-            
- };
-
 
 function onFormSubmit (e) {
         e.preventDefault();
-        e.currentTarget.reset();
-        localStorage.removeItem(LOCALSTORAGE_KEY_FORM);
-        console.log(formData); 
+        console.log(formData);
+        localStorage.removeItem(LOCALSTORAGE_KEY_FORM); 
+        e.target.reset(); 
 }
  
